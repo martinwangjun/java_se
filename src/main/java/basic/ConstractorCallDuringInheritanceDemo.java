30ee9be74783588597fcdb09cd156cca09b7debe
Martin Wang <martinwangjun@163.com>
2017-12-04 13:16:57 +0800

    构造函数的调用顺序

---
+++ src/main/java/basic/ConstractorCallDuringInheritanceDemo.java
@@ -0,0 +1,32 @@
+package basic;
+
+/**
+ * 构造函数是先从基类开始的。
+ * 
+ * @see Think in Java P244
+ * @author martin.wang
+ *
+ */
+class Art{
+    public Art() {
+        System.out.println("Art constractor.");
+    }
+}
+
+class Drawing extends Art{
+    public Drawing() {
+        System.out.println("Drawing constrator.");
+    }
+}
+
+class Cartoon extends Drawing{
+    public Cartoon() {
+        System.out.println("Cartoon constrator.");
+    }
+}
+
+public class ConstractorCallDuringInheritanceDemo {
+    public static void main(String[] args) {
+        new Cartoon();
+    }
+}
--- src/main/java/basic/ConstructorPriorityTest.java
+++ src/main/java/basic/ConstructorPriorityTest.java
@@ -1,7 +1,7 @@
 package basic;
 
 public class ConstructorPriorityTest {
-    
+    private static int staticVal = 1;
     private int qty = 1111;
     
     {
@@ -10,15 +10,29 @@ public class ConstructorPriorityTest {
         qty = 2222;
         System.out.println("初始化块执行后， qty = " + qty);
     }
+
+    {
+        System.out.println("静态初始化块执行前， staticVal = " + staticVal);
+        System.out.println("静态初始化块执行赋值");
+        qty = 2;
+        System.out.println("静态初始化块执行后， staticVal = " + staticVal);
+    }    
     
     public ConstructorPriorityTest() {
         System.out.println("构造函数执行前， qty = " + qty);
         System.out.println("构造函数执行赋值");
         qty = 3333;
         System.out.println("构造函数执行后， qty = " + qty);
+        
+        System.out.println("静态构造函数执行前， staticVal = " + staticVal);
+        System.out.println("静态构造函数执行赋值");
+        staticVal = 3;
+        System.out.println("静态构造函数执行后， staticVal = " + staticVal);
     }
     
     public static void main(String[] args) {
+        new ConstructorPriorityTest();
+        System.out.println("- - - - - -");
         new ConstructorPriorityTest();
     }
 }
